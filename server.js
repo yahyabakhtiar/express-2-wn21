eb76e591dd30bacff8049c38f0ea030f1fb5c424
Yahya Bakhtiar <yahyabakhtiar333@gmail.com>
2021-03-15 23:27:06 -0700

    adds new files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine','ejs');//sent views to EJS
 //this is how a route is built in Express
 app.get('/', (req,res)=>{
   var title = "Our Home Page";
-  res.render('index',{title:title});
+  res.render('pages/index',{title:title});
 });
 
 app.listen(PORT,()=>{
